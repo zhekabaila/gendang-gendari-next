40e750bee364e5df5b1d501bed865503a510b774
zhekabaila <zhekabaila@gmail.com>
2025-11-21 08:22:56 +0700

    feat: init

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from 'react'
 import { HomeLayout } from './_layouts/home-layout'
 
 function HomePage() {
-  return <HomeLayout />
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <HomeLayout />
+    </Suspense>
+  )
 }
 
 export default HomePage
